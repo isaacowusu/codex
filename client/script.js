c38404791eced62edee121706163a1c92c3538bd
Isaac Owusu <iowusu64749@gmail.com>
2023-03-16 00:10:06 +0000

    update the server url

--- client/script.js
+++ client/script.js
@@ -80,7 +80,7 @@ const handleSubmit = async (e) => {
 
     // fetch data from server -> bot's response
 
-    const response = await fetch('https://codex-roh8.onrender.com/', {
+    const response = await fetch('https://codex-roh8.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
